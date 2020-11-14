ed70a5fbeff49e6537140e5b919de6542d017899
Dell <mollanoman2019@gmail.com>
2020-11-14 09:33:01 +0600

    Image upload with volly -v1

---
+++ app/src/main/java/com/example/imageuploadwithvolly/MainActivity.java
@@ -0,0 +1,144 @@
+package com.example.imageuploadwithvolly;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Base64;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageView imageView;
+    Button selectButton,uploadButton;
+    Bitmap bitmap;
+    String encodedImage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.imageId);
+        selectButton=findViewById(R.id.selectImageId);
+        uploadButton=findViewById(R.id.uploadImageId);
+        selectButton.setOnClickListener(this);
+        uploadButton.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.selectImageId){
+            selectImage();
+        }
+        if (v.getId()==R.id.uploadImageId){
+            uploadImage();
+            
+        }
+    }
+
+    private void uploadImage() {
+        RequestQueue requestQueue= Volley.newRequestQueue(MainActivity.this);
+        String baseUrl="https://my-json-pratice.000webhostapp.com/BlueSky/postImage.php";
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, baseUrl, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(MainActivity.this, ""+response, Toast.LENGTH_SHORT).show();
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this, ""+error, Toast.LENGTH_SHORT).show();
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> map=new HashMap<>();
+                map.put("image",encodedImage);
+                return super.getParams();
+            }
+        };
+        requestQueue.add(stringRequest);
+    }
+
+    private void selectImage() {
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override public void onPermissionGranted(PermissionGrantedResponse response) {
+
+                        Intent intent=new Intent(Intent.ACTION_PICK);
+                        intent.setType("image/*");
+                        startActivityForResult(Intent.createChooser(intent,"Select image"),1);
+
+                        }
+                    @Override public void onPermissionDenied(PermissionDeniedResponse response) {
+
+
+                    }
+                    @Override public void onPermissionRationaleShouldBeShown(PermissionRequest permission, PermissionToken token) {
+                        token.continuePermissionRequest();
+
+                    }
+                }).check();
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null){
+            Uri filePath=data.getData();
+            try {
+                InputStream inputStream=getContentResolver().openInputStream(filePath);
+                bitmap= BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(bitmap);
+                imageStore(bitmap);
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+
+    }
+
+    private void imageStore(Bitmap bitmap) {
+        ByteArrayOutputStream stream=new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG,100,stream);
+        byte[]imageBytes=stream.toByteArray();
+        encodedImage=android.util.Base64.encodeToString(imageBytes, Base64.DEFAULT);
+
+
+    }
+}
